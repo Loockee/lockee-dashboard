ed46df90ba0790f06ff5542c1857b6187ba971fe
lefebvrm <mlefebvre@groupemutuel.ch>
2019-10-11 13:11:56 +0200

    chore: cleanup main page
    Signed-off-by: lefebvrm <mlefebvre@groupemutuel.ch>

--- src/main.js
+++ src/main.js
@@ -1,54 +1,57 @@
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import App from './App';
 
 // BootstrapVue add
-import BootstrapVue from 'bootstrap-vue'
+import BootstrapVue from 'bootstrap-vue';
 // Router & Store add
-import router from './router'
-import { store } from './store'
+import router from './router';
+import { store } from './store';
 // Multi Language Add
-import en from './locales/en.json'
-import es from './locales/es.json'
-import VueI18n from 'vue-i18n'
-import { defaultLocale, localeOptions, firebaseConfig } from './constants/config'
+import en from './locales/en.json';
+import es from './locales/es.json';
+import it from './locales/it.json';
+import fr from './locales/de.json';
+import de from './locales/fr.json';
+import VueI18n from 'vue-i18n';
+import { defaultLocale, localeOptions, firebaseConfig } from './constants/config';
 // Notification Component Add
-import Notifications from './components/Common/Notification'
+import Notifications from './components/Common/Notification';
 // Breadcrumb Component Add
-import Breadcrumb from './components/Common/Breadcrumb'
+import Breadcrumb from './components/Common/Breadcrumb';
 // RefreshButton Component Add
-import RefreshButton from './components/Common/RefreshButton'
+import RefreshButton from './components/Common/RefreshButton';
 // Colxx Component Add
-import Colxx from './components/Common/Colxx'
+import Colxx from './components/Common/Colxx';
 // Perfect Scrollbar Add
-import vuePerfectScrollbar from 'vue-perfect-scrollbar'
-import contentmenu from 'v-contextmenu'
-import lineClamp from 'vue-line-clamp'
-import VCalendar from 'v-calendar'
-import 'v-calendar/lib/v-calendar.min.css'
-import VueScrollTo from 'vue-scrollto'
-import firebase from 'firebase/app'
-import 'firebase/auth'
+import vuePerfectScrollbar from 'vue-perfect-scrollbar';
+import contentmenu from 'v-contextmenu';
+import lineClamp from 'vue-line-clamp';
+import VCalendar from 'v-calendar';
+import 'v-calendar/lib/v-calendar.min.css';
+import VueScrollTo from 'vue-scrollto';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 
-Vue.use(BootstrapVue)
-Vue.use(VueI18n)
+Vue.use(BootstrapVue);
+Vue.use(VueI18n);
 
-const messages = { en: en, es: es }
+const messages = { en, es, fr, it, de };
 const locale = (localStorage.getItem('currentLanguage') && localeOptions.filter(x => x.id === localStorage.getItem('currentLanguage')).length > 0) ? localStorage.getItem('currentLanguage') : defaultLocale
 const i18n = new VueI18n({
   locale: locale,
   fallbackLocale: 'en',
   messages
-})
+});
 
-Vue.use(Notifications)
-Vue.component('piaf-breadcrumb', Breadcrumb)
-Vue.component('b-refresh-button', RefreshButton)
-Vue.component('b-colxx', Colxx)
-Vue.component('vue-perfect-scrollbar', vuePerfectScrollbar)
-Vue.use(require('vue-shortkey'))
-Vue.use(contentmenu)
-Vue.use(lineClamp, { /* plugin options */ })
+Vue.use(Notifications);
+Vue.component('piaf-breadcrumb', Breadcrumb);
+Vue.component('b-refresh-button', RefreshButton);
+Vue.component('b-colxx', Colxx);
+Vue.component('vue-perfect-scrollbar', vuePerfectScrollbar);
+Vue.use(require('vue-shortkey'));
+Vue.use(contentmenu);
+Vue.use(lineClamp, { /* plugin options */ });
 Vue.use(VCalendar, {
   firstDayOfWeek: 2, // ...other defaults,
   formats: {
@@ -61,10 +64,10 @@ Vue.use(VCalendar, {
   datePickerShowDayPopover: false,
   popoverExpanded: true,
   popoverDirection: 'bottom'
-})
-Vue.use(VueScrollTo)
+});
+Vue.use(VueScrollTo);
 
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 
 export default new Vue({
   el: '#app',
@@ -72,4 +75,4 @@ export default new Vue({
   router,
   store,
   render: h => h(App)
-})
+});
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,52 +1,50 @@
-import { defaultDirection } from '../constants/config'
+import { defaultDirection } from '../constants/config';
 
 export const addCommas = nStr => {
-  nStr += ''
-  var x = nStr.split('.')
-  var x1 = x[0]
-  var x2 = x.length > 1 ? '.' + x[1] : ''
-  var rgx = /(\d+)(\d{3})/
+  nStr += '';
+  const x = nStr.split('.');
+  let x1 = x[0];
+  const x2 = x.length > 1 ? '.' + x[1] : '';
+  const rgx = /(\d+)(\d{3})/;
   while (rgx.test(x1)) {
     x1 = x1.replace(rgx, '$1' + ',' + '$2')
   }
-  return x1 + x2
-}
+  return x1 + x2;
+};
 
 export const mapOrder = (array, order, key) => {
   array.sort(function (a, b) {
-    var A = a[key]
-    var B = b[key]
+    const A = a[key];
+    const B = b[key];
     if (order.indexOf(A + '') > order.indexOf(B + '')) {
-      return 1
+      return 1;
     } else {
-      return -1
+      return -1;
     }
-  })
+  });
   return array
-}
+};
+
+const sanitize = (date) => {
+  const prefix = (date < 10) ? '0' : '';
+  return `${prefix}${date}`;
+};
 
 export const getDateWithFormat = () => {
-  const today = new Date()
-  let dd = today.getDate()
-  let mm = today.getMonth() + 1 // January is 0!
-
-  var yyyy = today.getFullYear()
-  if (dd < 10) {
-    dd = '0' + dd
-  }
-  if (mm < 10) {
-    mm = '0' + mm
-  }
-  return dd + '.' + mm + '.' + yyyy
-}
+  const today = new Date();
+  const dd = sanitize(today.getDate());
+  const mm = sanitize(today.getMonth() + 1); // January is 0!
+  const yyyy = today.getFullYear();
+  return `${dd}.${mm}.${yyyy}`
+};
 
 export const getCurrentTime = () => {
-  const now = new Date()
-  return now.getHours() + ':' + now.getMinutes()
-}
+  const now = new Date();
+  return now.getHours() + ':' + now.getMinutes();
+};
 
 export const ThemeColors = () => {
-  let rootStyle = getComputedStyle(document.body)
+  let rootStyle = getComputedStyle(document.body);
   return {
     themeColor1: rootStyle.getPropertyValue('--theme-color-1').trim(),
     themeColor2: rootStyle.getPropertyValue('--theme-color-2').trim(),
@@ -64,7 +62,7 @@ export const ThemeColors = () => {
     foregroundColor: rootStyle.getPropertyValue('--foreground-color').trim(),
     separatorColor: rootStyle.getPropertyValue('--separator-color').trim()
   }
-}
+};
 
 export const chartTooltip = {
   backgroundColor: ThemeColors().foregroundColor,
@@ -76,88 +74,92 @@ export const chartTooltip = {
   xPadding: 15,
   yPadding: 15,
   cornerRadius: 0.15
-}
+};
 
 export const centerTextPlugin = {
+
   afterDatasetsUpdate: function (chart) {},
+
   beforeDraw: function (chart) {
-    var width = chart.chartArea.right
-    var height = chart.chartArea.bottom
-    var ctx = chart.chart.ctx
-    ctx.restore()
+    const width = chart.chartArea.right;
+    const height = chart.chartArea.bottom;
+    const ctx = chart.chart.ctx;
+    ctx.restore();
 
-    var activeLabel = chart.data.labels[0]
-    var activeValue = chart.data.datasets[0].data[0]
-    var dataset = chart.data.datasets[0]
-    var meta = dataset._meta[Object.keys(dataset._meta)[0]]
-    var total = meta.total
+    let activeLabel = chart.data.labels[0];
+    let activeValue = chart.data.datasets[0].data[0];
+    let dataset = chart.data.datasets[0];
+    let meta = dataset._meta[Object.keys(dataset._meta)[0]];
+    let total = meta.total;
 
-    var activePercentage = parseFloat(((activeValue / total) * 100).toFixed(1))
+    let activePercentage = parseFloat(((activeValue / total) * 100).toFixed(1));
     activePercentage = chart.legend.legendItems[0].hidden
       ? 0
-      : activePercentage
+      : activePercentage;
 
     if (chart.pointAvailable) {
-      activeLabel = chart.data.labels[chart.pointIndex]
+      activeLabel = chart.data.labels[chart.pointIndex];
       activeValue =
-        chart.data.datasets[chart.pointDataIndex].data[chart.pointIndex]
+        chart.data.datasets[chart.pointDataIndex].data[chart.pointIndex];
 
-      dataset = chart.data.datasets[chart.pointDataIndex]
-      meta = dataset._meta[Object.keys(dataset._meta)[0]]
-      total = meta.total
-      activePercentage = parseFloat(((activeValue / total) * 100).toFixed(1))
+      dataset = chart.data.datasets[chart.pointDataIndex];
+      meta = dataset._meta[Object.keys(dataset._meta)[0]];
+      total = meta.total;
+      activePercentage = parseFloat(((activeValue / total) * 100).toFixed(1));
       activePercentage = chart.legend.legendItems[chart.pointIndex].hidden
         ? 0
-        : activePercentage
+        : activePercentage;
     }
 
-    ctx.font = '36px Nunito, sans-serif'
-    ctx.fillStyle = ThemeColors().primaryColor
-    ctx.textBaseline = 'middle'
+    ctx.font = '36px Nunito, sans-serif';
+    ctx.fillStyle = ThemeColors().primaryColor;
+    ctx.textBaseline = 'middle';
 
-    var text = activePercentage + '%'
-    var textX = Math.round((width - ctx.measureText(text).width) / 2)
-    var textY = height / 2
-    ctx.fillText(text, textX, textY)
+    const text = activePercentage + '%';
+    const textX = Math.round((width - ctx.measureText(text).width) / 2);
+    const textY = height / 2;
+    ctx.fillText(text, textX, textY);
 
-    ctx.font = '14px Nunito, sans-serif'
-    ctx.textBaseline = 'middle'
+    ctx.font = '14px Nunito, sans-serif';
+    ctx.textBaseline = 'middle';
 
-    var text2 = activeLabel
-    var textX2 = Math.round((width - ctx.measureText(text2).width) / 2)
-    var textY2 = height / 2 - 30
-    ctx.fillText(text2, textX2, textY2)
+    const text2 = activeLabel;
+    const textX2 = Math.round((width - ctx.measureText(text2).width) / 2);
+    const textY2 = height / 2 - 30;
+    ctx.fillText(text2, textX2, textY2);
 
-    ctx.save()
+    ctx.save();
   },
+
   beforeEvent: function (chart, event, options) {
-    var firstPoint = chart.getElementAtEvent(event)[0]
+    const firstPoint = chart.getElementAtEvent(event)[0];
 
     if (firstPoint) {
-      chart.pointIndex = firstPoint._index
-      chart.pointDataIndex = firstPoint._datasetIndex
-      chart.pointAvailable = true
+      chart.pointIndex = firstPoint._index;
+      chart.pointDataIndex = firstPoint._datasetIndex;
+      chart.pointAvailable = true;
     }
   }
-}
+};
+
 export const getDirection = () => {
-  let direction = defaultDirection
+  let direction = defaultDirection;
   if (localStorage.getItem('direction')) {
     const localValue = localStorage.getItem('direction')
     if (localValue === 'rtl' || localValue === 'ltr') {
-      direction = localValue
+      direction = localValue;
     }
   }
   return {
     direction,
-    isRtl: direction === 'rtl'
+    isRtl: direction === 'rtl',
   }
-}
+};
 
 export const setDirection = localValue => {
-  let direction = 'ltr'
+  let direction = 'ltr';
   if (localValue === 'rtl' || localValue === 'ltr') {
-    direction = localValue
+    direction = localValue;
   }
-  localStorage.setItem('direction', direction)
-}
+  localStorage.setItem('direction', direction);
+};
